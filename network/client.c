05c1e97ed91160a2ee371b07beecec6c0291e93e
cyliu <cyliu@debian>
2016-08-19 14:53:29 +0800

    update client.c

--- network/client.c
+++ network/client.c
@@ -43,7 +43,8 @@ int main(int argc, char *argv[])
         error("ERROR connecting");
     printf("Please enter the message: ");
     bzero(buffer,256);
-    fgets(buffer,255,stdin);
+    sprintf(buffer,"I am a client\n");
+    //fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
          error("ERROR writing to socket");
