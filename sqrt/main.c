369870a2c386628843648f6faacd920e5ea38f3b
cyliustack <cyliustack@gmail.com>
2016-10-05 19:35:29 +0800

    from sqrt to compute

--- sqrt/main.c
+++ sqrt/main.c
@@ -5,6 +5,7 @@
 int main(int argc, char* argv[]){
     int i,j;
     int n_loop;
+    double res=0;
 
     if(argc<2){
         printf("usage : ./sqrt n_loop\n");
@@ -13,7 +14,7 @@ int main(int argc, char* argv[]){
     n_loop = atoi(argv[1]);
 
     for(i=0;i<n_loop;i++){
-        sqrt(i*i+0.1*i);
+        res = (i*i+0.1*i+(i*i)%10);
     }
 
     return 0;
