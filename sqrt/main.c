b909b8a809b3dc66e1af38488725ae929917d5c2
cyliu <cyliu@debian>
2016-10-10 23:04:50 +0800

    merge upstream for sqrt, and then make sqrt more complex by modulo

--- sqrt/main.c
+++ sqrt/main.c
@@ -8,14 +8,14 @@ int main(int argc, char* argv[]){
     double res=0;
 
     if(argc<2){
-        printf("usage : ./compute n_loop\n");
+        printf("Usage : ./compute n_loop\n");
         return -1;
     }
 
     n_loop = atoi(argv[1]);
 
     for(i=0;i<n_loop;i++){
-        res = sqrt(i*i+0.1);
+        res = sqrt(i*i+0.1+(i*i)%13);
     }
 
     return 0;
